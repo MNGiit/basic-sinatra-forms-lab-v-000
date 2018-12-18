3c73b6635b2ece613622c789a85861c4a65ffad6
Mohammed Noori <kmanoorib@gmail.com>
2018-12-18 22:53:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,12 @@ class App < Sinatra::Base
   end
   
   post "/newteam" do
-    
+      @team_name = params[:team_name]
+      <p>Coach: <input type="text" name="coach"></p>
+      <p>Point Guard: <input type="text" name="point_guard"></p>
+      <p>Shooting Guard: <input type="text" name="shooting_guard"></p>
+      <p>Small Foward: <input type="text" name="small_foward"></p>
+      <p>Power Forward: <input type="text" name="power_forward"></p>
+      <p>Center: <input type="text" name="center"></p>
   
 end
